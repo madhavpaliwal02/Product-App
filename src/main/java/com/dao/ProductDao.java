ce4578bd0333c8b9225ab7234fb843142c62ff90
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-07-06 19:12:37 +0530

    Completely configured Controller-Entity-XML

---
+++ src/main/java/com/dao/ProductDao.java
@@ -0,0 +1,67 @@
+package com.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.entities.Product;
+
+@Component
+public class ProductDao {
+
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	// create
+	@Transactional
+	public void createProduct(Product product) {
+		this.hibernateTemplate.save(product);
+	}
+	
+	// get all products
+	public List<Product> getAllProducts() {
+		return (List<Product>)this.hibernateTemplate.loadAll(Product.class);
+	}
+	
+	// get a product
+	public Product getProduct(int pid) {
+		return (Product)this.hibernateTemplate.get(Product.class, pid);
+	}
+	
+	
+	public void deleteProduct(int pid) {
+		this.hibernateTemplate.delete(getProduct(pid));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
