6ee86194988aa2fe34ed4fc5cd7810d6c09cadeb
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-07-07 18:30:53 +0530

    Added dashboard to the home page

--- src/main/java/com/controller/HomeCtrl.java
+++ src/main/java/com/controller/HomeCtrl.java
@@ -1,14 +1,33 @@
 package com.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model; 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.dao.ProductDao;
+import com.entities.Product;
+
 
 @Controller
 public class HomeCtrl {
 
+	@Autowired
+	private ProductDao productDao;
+
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model m) {
+		
+		List<Product> products = this.productDao.getAllProducts();
+		m.addAttribute("product", products);
+		
 		return "index";
 	}
 
@@ -18,4 +37,23 @@ public class HomeCtrl {
 		return "add_product_form";
 	}
 
+	// handle add product form
+	@RequestMapping(value = "/handle-product", method = RequestMethod.POST)
+	public RedirectView handleProduct(@ModelAttribute Product product, HttpServletRequest request) {
+		
+		System.out.println(product);
+		this.productDao.createProduct(product);
+		
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(request.getContextPath() + "/");
+		return redirectView;
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
